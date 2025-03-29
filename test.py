17b90ac36b609b41770646b930e575bf96c333a2
ahmetozdemirrr <ao7252167@gmail.com>
2025-03-29 23:15:45 +0300

    Initial commit for version 0.1.1 with with_punctuation parameter

---
+++ test.py
@@ -0,0 +1,4 @@
+from turkish_syllable import syllabify
+
+print(syllabify("halkların astların testlerin bantlamak da gerekir kelimesi budur, ardından neler gelir bilemiyorum ChatGPT!", with_punctuation=False))
+print(syllabify("test kelimesi budur, ardından neler gelir bilemiyorum ChatGPT!", with_punctuation=True))
